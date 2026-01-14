24fe778313412678b3d7b88fbf4703dbd875feaa
developerriyansh-flow2125 <developerriyansh@gmail.com>
2026-01-14 20:17:41 +0530

    day 1 - 12/01/2026

---
+++ day1.js
@@ -0,0 +1,14 @@
+console.log(null === null); // true --> comparable
+console.log(null === undefined); // false  
+console.log(undefined === undefined); // true --> comparable
+console.log(undefined == undefined); // true --> comparable
+console.log(null == null); // true --> comparable
+console.log(NaN === NaN); // false --> special values
+console.log(NaN == NaN); // false --> special values
+console.log([] == []); // false --> different memory locations
+console.log([] === []); // false --> different memory locations
+console.log({} === {}); // false --> different memory locations
+console.log({} == {}); // false --> different memory locations
+console.log(true === 1); // false --> different type boolean != number
+console.log(true === true); // true --> comparable
+console.log(false === false); // true --> comparable
